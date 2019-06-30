c24a93159eb9ae7ec3370ac1e30258b4785d26f0
DeliaCodes <delia@delia.codes>
2019-06-29 21:38:07 -0400

    Drafting Tests.

---
+++ test-tickets.js
@@ -0,0 +1,9 @@
+const assert = require("assert");
+
+const { generateTicket, generateOutput, generateLine } = require("./tickets");
+
+describe("generate ticket", function() {
+  it("should return a new ticket with 2 lines", function() {
+    // assert.generateTicket(2).shouldReturnAnObject
+  });
+});
